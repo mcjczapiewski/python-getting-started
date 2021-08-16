c755022455d846c61688447792c9d8690edc5c36
Maciej Czapiewski <mcjczapiewski@gmail.com>
2021-08-16 19:49:45 +0200

    Change BooksModel

--- hello/filters.py
+++ hello/filters.py
@@ -5,25 +5,25 @@ from django_filters import DateFilter, CharFilter
 
 class BooksFilter(django_filters.FilterSet):
     title = CharFilter(
-        field_name="title", lookup_expr="icontains", label="Tytuł:"
+        field_name="title", lookup_expr="icontains", label="Tytuł"
     )
     author = CharFilter(
-        field_name="author", lookup_expr="icontains", label="Autor:"
+        field_name="author", lookup_expr="icontains", label="Autor"
     )
     publication_language = CharFilter(
         field_name="publication_language",
         lookup_expr="icontains",
-        label="Język publikacji:",
+        label="Język publikacji",
     )
     start_date = DateFilter(
         field_name="publication_date",
         lookup_expr="gte",
-        label="Od daty:",
+        label="Od daty",
         widget=DateInput(attrs={"type": "date"}),
     )
     end_date = DateFilter(
         field_name="publication_date",
         lookup_expr="lte",
-        label="Do daty:",
+        label="Do daty",
         widget=DateInput(attrs={"type": "date"}),
     )
--- hello/models.py
+++ hello/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 
 
-# Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField("date created", auto_now_add=True)
-
-
 class BooksModel(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField("Tytuł", max_length=255)
     author = models.CharField("Autor", max_length=255)
-    publication_date = models.DateField("Data publikacji")
-    isbn_number = models.IntegerField("Numer ISBN")
-    pages_count = models.IntegerField("Liczba stron")
-    cover_url = models.CharField("Link do okładki", max_length=255)
-    publication_language = models.CharField("Język publikacji", max_length=255)
+    publication_date = models.CharField(
+        "Data publikacji", max_length=50, null=True, blank=True
+    )
+    isbn_number = models.CharField(
+        "Numer ISBN", max_length=100, null=True, blank=True
+    )
+    pages_count = models.IntegerField("Liczba stron", null=True, blank=True)
+    cover_url = models.URLField(
+        "Link do okładki", max_length=255, null=True, blank=True
+    )
+    publication_language = models.CharField(
+        "Język publikacji", max_length=255, null=True, blank=True
+    )
 
     def __str__(self):
         return self.title
