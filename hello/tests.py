865f33b190e768c793c3da8e3f22593b50b771ac
Maciej Czapiewski <mcjczapiewski@gmail.com>
2021-08-19 13:31:36 +0200

    Take on tests

--- hello/tests.py
+++ hello/tests.py
@@ -1,19 +1,50 @@
-from django.contrib.auth.models import AnonymousUser
 from django.test import TestCase, RequestFactory
 
-from .views import index
+from .views import all_books
+from .views import delete_book
+from .models import BooksModel
+from .forms import BooksForm
 
 
 class SimpleTest(TestCase):
     def setUp(self):
-        # Every test needs access to the request factory.
         self.factory = RequestFactory()
+        book = BooksModel()
+        book.title = "Additional"
+        book.author = "Anonymous"
+        book.save()
+        self.book = book
 
-    def test_details(self):
-        # Create an instance of a GET request.
+        form = BooksForm()
+        form.title = "new book"
+        form.author = "author"
+        self.form = form
+
+    def test_calling_main_page_with_success(self):
         request = self.factory.get("/")
-        request.user = AnonymousUser()
+        response = all_books(request)
+        self.assertTrue(response.status_code == 200)
 
-        # Test my_view() as if it were deployed at /customer/details
-        response = index(request)
-        self.assertEqual(response.status_code, 200)
+    def test_book_is_properly_created(self):
+        record = BooksModel.objects.get(pk=1)
+        self.assertEqual(record, self.book)
+
+    def test_books_count_equal_one(self):
+        books_count = BooksModel.objects.all().count()
+        self.assertEqual(books_count, 1)
+
+    def test_books_form(self):
+        self.assertEqual(self.form.title, "new book")
+
+    def test_form_redirect_to_all_books_page(self):
+        single_book = {"title": "new book", "author": "author"}
+        response = self.client.post("/create-edit/", single_book, follow=True)
+        response_path = response.request.get("PATH_INFO")
+        self.assertEqual(response_path, "/")
+
+    def test_book_is_deleted_redirect_after(self):
+        response = delete_book(self, 1)
+        books_count = BooksModel.objects.all().count()
+        response_path = response.url
+        self.assertEqual(books_count, 0)
+        self.assertEqual(response_path, "/")
